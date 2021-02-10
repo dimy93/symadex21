da640060da0e196797096bea9f91c6bdfc58c577
dimitadi <dimitadi>
2021-02-10 19:05:52 +0100

    eot

---
+++ 3d_convBig_rotation_EoT.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 . --netname ../nets/convBigRELU__DiffAI.pyt --geometric --geometric_config ./3d_rotation.txt --num_params 3 --dataset mnist --attack --skip_geom_ver --from_test 0 --num_tests 100 --eot > 3d_rotation_convBig_0_100.txt
--- 3d_convBig_translation_085.sh
+++ 3d_convBig_translation_085.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3 . --netname ../nets/convBigRELU__DiffAI.pyt --geometric --geometric_config 3d_translation.txt --num_params 3 --dataset mnist --attack --skip_geom_ver --from_test 0 --num_tests 100 --geom_box 2 1 1 --geom_baseline 4 2 2 --c 0.85 > 3d_translation_convBig_0_100_085.txt
+python3 . --netname ../nets/convBigRELU__DiffAI.pyt --geometric --geometric_config 3d_translation.txt --num_params 3 --dataset mnist --attack --skip_geom_ver --from_test 0 --num_tests 100 --geom_box 2 1 1 --c 0.85 > 3d_translation_convBig_0_100_085.txt
